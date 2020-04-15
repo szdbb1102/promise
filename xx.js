d8d77830fbbf2381658a54fabd121072289ce4fb
xuexin <xuexin@cloudwalk.cn>
2020-04-15 16:04:18 +0800

    大量报错

---
+++ xx.js
@@ -0,0 +1,175 @@
+const PENDING = "pending";
+const FULLFILLED = "pending";
+const REJECTED = "rejected";
+
+function resolvePromise(promise2, x, resolve, reject) {
+  if (promise2 === x) {
+    return reject(new TypeError("Chaining cycle detected for promise"));
+  }
+  let called = false;
+  if (x !== null && (typeof x === "object" || typeof x === "function")) {
+    try {
+      let then = x.then;
+      if (typeof then === "function") {
+        then.call(
+          x,
+          (y) => {
+            if (called) return;
+            called = true;
+            resolvePromise(promise2, y, resolve, reject);
+          },
+          (e) => {
+            if (called) return;
+            called = true;
+            reject(e);
+          }
+        );
+      } else {
+        resolve(x);
+      }
+    } catch (error) {
+      if (called) return;
+      called = true;
+      reject(error);
+    }
+  } else {
+    resolve(x);
+  }
+}
+class Promise {
+  constructor(excutor) {
+    let resolve = (val) => {
+      if (this.state === PENDING) {
+        this.value = val;
+        this.state = FULLFILLED;
+        this.fullfilledCB.forEach((v) => v());
+      }
+    };
+    let reject = (e) => {
+      if (this.state === PENDING) {
+        this.state = REJECTED;
+        this.reason = e;
+        this.rejectedCB.forEach((v) => v());
+      }
+    };
+    this.state = PENDING;
+    this.value = undefined;
+    this.reason = undefined;
+    this.fullfilledCB = [];
+    this.rejectedCB = [];
+    try {
+      excutor(resolve, reject);
+    } catch (error) {
+      reject(error);
+    }
+  }
+  then(fullfilled, rejected) {
+    fullfilled = typeof fullfilled === "function" ? fullfilled : (val) => val;
+    rejected =
+      typeof fullfilled === "function"
+        ? rejected
+        : (error) => {
+            throw error;
+          };
+    let promise2 = new Promise((resolve, reject) => {
+      if (this.state === FULLFILLED) {
+        setTimeout(() => {
+          try {
+            let x = fullfilled(this.value);
+            resolvePromise(promise2, x, resolve, reject);
+          } catch (error) {
+            reject(error);
+          }
+        }, 0);
+      }
+      if (this.state === REJECTED) {
+        setTimeout(() => {
+          try {
+            let x = reject(this.reason);
+            resolvePromise(promise2, x, resolve, reject);
+          } catch (error) {
+            reject(error);
+          }
+        }, 0);
+      }
+      if (this.state === PENDING) {
+        this.fullfilledCB.push(() => {
+          setTimeout(() => {
+            try {
+              let x = fullfilled(this.value);
+              resolvePromise(promise2, x, resolve, reject);
+            } catch (error) {
+              reject(error);
+            }
+          }, 0);
+        });
+        this.rejectedCB.push(() => {
+          setTimeout(() => {
+            try {
+              let x = reject(this.reason);
+              resolvePromise(promise2, x, resolve, reject);
+            } catch (error) {
+              reject(error);
+            }
+          }, 0);
+        });
+      }
+    });
+    return promise2;
+  }
+  catch(fn) {
+    return this.then(null, fn);
+  }
+}
+
+//resolve方法
+Promise.resolve = function (val) {
+  return new Promise((resolve, reject) => {
+    resolve(val);
+  });
+};
+//reject方法
+Promise.reject = function (val) {
+  return new Promise((resolve, reject) => {
+    reject(val);
+  });
+};
+//race方法
+Promise.race = function (promises) {
+  return new Promise((resolve, reject) => {
+    for (let i = 0; i < promises.length; i++) {
+      promises[i].then(resolve, reject);
+    }
+  });
+};
+//all方法(获取所有的promise，都执行then，把结果放到数组，一起返回)
+Promise.all = function (promises) {
+  let arr = [];
+  let i = 0;
+  function processData(index, data) {
+    arr[index] = data;
+    i++;
+    if (i == promises.length) {
+      resolve(arr);
+    }
+  }
+  return new Promise((resolve, reject) => {
+    for (let i = 0; i < promises.length; i++) {
+      promises[i].then((data) => {
+        processData(i, data);
+      }, reject);
+    }
+  });
+};
+
+// 执行测试用例需要用到的代码
+Promise.deferred = function () {
+  let defer = {};
+  defer.promise = new Promise((resolve, reject) => {
+    defer.resolve = resolve;
+    defer.reject = reject;
+  });
+  return defer;
+};
+console.log(module, module.exports, Promise);
+module.exports = Promise;
